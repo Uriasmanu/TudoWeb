edf41d8e50c94539c75a6ffc72d8d27d18485039
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-09-16 20:21:02 -0300

    alteração

--- src/App.java
+++ src/App.java
@@ -1,33 +1,50 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
-public class PesquisaDeOpiniao {
+public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int excelenteCount = 0;
-        int ruimCount = 0;
 
-        for (int i = 1; i <= 10; i++) {
-            System.out.println("Entrevistado " + i);
-            System.out.print("Nome: ");
-            String nome = scanner.nextLine();
-            System.out.print("Idade: ");
-            int idade = scanner.nextInt();
-            scanner.nextLine();  // Limpar o buffer de entrada
-            System.out.print("Opinião (1-EXCELENTE, 2-BOM, 3-RUIM): ");
-            int opiniao = scanner.nextInt();
-            scanner.nextLine();  // Limpar o buffer de entrada
+        String nome, opniao;
+        int idade, exelente, bom, ruim;
 
-            if (opiniao == 1) {
-                excelenteCount++;
-            } else if (opiniao == 3) {
-                ruimCount++;
+        exelente = 0;
+        ruim = 0;
+
+        JOptionPane.showMessageDialog(null, "Seja bem-vindo(a) a TudoWeb");
+
+        for (int i = 0; i <= 10; i++) { // Correção aqui: ponto e vírgula em vez de vírgula
+
+            nome = JOptionPane.showInputDialog(null, "Por gentileza digite o seu nome:");
+            idade = Integer.parseInt(JOptionPane.showInputDialog(null, "Por gentileza digite o sua idade:"));
+
+            opniao = JOptionPane.showInputDialog(null, "Sr(a). " + nome + ", poderia nos dar a sua opinião sobre o nosso site:" + "\n1 - Excelente"
+                    + "\n2 - Bom" + "\n3 - Ruim");
+
+            int opniaoInt = Integer.parseInt(opniao);
+
+            switch (opniaoInt) {
+                case 1:
+                    JOptionPane.showMessageDialog(null, "Entrevistado" + i + "escolheu a opção 1. Excelente");
+                    exelente = exelente + 1;
+                    break;
+
+                case 2:
+                    JOptionPane.showMessageDialog(null, "Entrevistado" + i + "escolheu a opção 2 - Bom");
+                    break;
+
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Entrevistado" + i + "escolheu a opção 3 - Ruim");
+                    ruim = ruim + 1;
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Opção inválida");
+                    break;
             }
+
         }
 
-        System.out.println("\nResultados da pesquisa:");
-        System.out.println("Quantidade de respostas \"EXCELENTE\": " + excelenteCount);
-        System.out.println("Quantidade de respostas \"RUIM\": " + ruimCount);
+        // Após o loop, você pode exibir os resultados, por exemplo:
+        JOptionPane.showMessageDialog(null, "Resultados:\nExcelente: " + exelente + " \nRuim" + ruim);
 
-        scanner.close();
     }
 }
