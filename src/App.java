76368a7166af7dfc575cf3bce9c0cd180a172c00
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-09-16 18:24:08 +0000

    .

--- src/App.java
+++ src/App.java
@@ -11,7 +11,26 @@ public class App {
         nome = JOptionPane.showInputDialog(null,"Por gentileza digite o seu nome:");
         idade = Integer.parseInt(JOptionPane.showInputDialog(null,"Por gentileza digite o sua idade:"));
 
-        JOptionPane.showMessageDialog(null, "Sr(a)." + nome + "poderia nos dar a sua opnião sobre o nosso site:" + "\n1 - Exelente" +
+        opniao = JOptionPane.showInputDialog(null, "Sr(a)." + nome + "poderia nos dar a sua opnião sobre o nosso site:" + "\n1 - Exelente" +
         "\n2 - Bom" + "\n3 - Ruim");
+
+        switch(opniao){
+            case 1:
+                JOptionPane.showMessageDialog(null,"Você escolheu a opção 1. Exelente");
+                break;
+
+            case 2:
+                JOptionPane.showMessageDialog(null,"Você escolheu a opção 2 - Bom");
+                break;
+            
+            case 3:
+                JOptionPane.showMessageDialog(null,"Você escolheu a opção 3 - Ruim");
+                break;    
+
+            default:
+            JOptionPane.showMessageDialog(null, "Opção invalida");
+            break;
+        }
+    
     }
 }
